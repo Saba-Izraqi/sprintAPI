a3203c6de8a139dc378678ac49b448813cb1b82e
Saba Izraqi <izraqisaba@gmail.com>
2025-04-11 21:13:20 -0400

    feat (src): start the server from src/index

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import startServer from "./API";
+
+// TODO: Implement the logic below.
+// Check DB Health
+// if(!database) {
+//   createDatabase();
+// }
+
+startServer(4000); // start the server on port 3000
