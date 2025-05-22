e94cac9177908916f16086fe6fc46ccae1433f3a
Saba Izraqi <izraqisaba@gmail.com>
2025-05-22 15:31:49 +0300

    chore: removed file path comment as per review

--- src/domain/IRepos/IUserRepo.ts
+++ src/domain/IRepos/IUserRepo.ts
@@ -1,11 +1,12 @@
-// src/domain/IRepos/IUserRepo.ts
 import { RegisterUserDto } from "../DTOs/userDTO";
 import { User } from "../entities";
 
 export interface IUserRepo {
   createUser(user: RegisterUserDto): any;
   findByEmail(email: string): Promise<any>;
-  updateEmailVerification(email: string, isEmailVerified: boolean): Promise<User>;
+  updateEmailVerification(
+    email: string,
+    isEmailVerified: boolean
+  ): Promise<User>;
   updatePassword(userId: string, newPassword: string): Promise<void>;
 }
-
