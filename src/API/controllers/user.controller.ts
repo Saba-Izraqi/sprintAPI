53763cfc1047f6074ff0b328efd3366098cae73a
Saba Izraqi <izraqisaba@gmail.com>
2025-05-22 03:37:31 +0300

    feat (controller): update controller to use error middleware and custom exceptions

--- src/API/controllers/user.controller.ts
+++ src/API/controllers/user.controller.ts
@@ -1,28 +1,28 @@
 import { injectable, inject } from "tsyringe";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { UserService } from "../../app/services/user.service";
 import { validate } from "class-validator";
-import {
-  RegisterUserDto,
-  LoginUserDto,
-  UserResponseDto,
-} from "../../domain/DTOs/userDTO";
+import { plainToInstance } from "class-transformer";
+import { RegisterUserDto, LoginUserDto } from "../../domain/DTOs/userDTO";
 import { Token } from "../enums/token";
 import { genToken } from "../utils/token";
 import PostmarkSender from "../../infrastructure/email/postmarkSender";
+import { UserError } from "../../app/exceptions";
 
 @injectable()
 export class UserController {
   constructor(@inject(UserService) private userService: UserService) {}
 
-  async registerUser(req: Request, res: Response) {
-    const dto = Object.assign(new RegisterUserDto(), req.body);
-    const errors = await validate(dto);
-    if (errors.length) {
-      res.status(400).json({ errors, success: false });
-      return;
-    }
+  async register(req: Request, res: Response, next: NextFunction) {
+    const dto = plainToInstance(RegisterUserDto, req.body);
+    //*I Wrapped validate() in try-catch to handle unexpected errors and ensure they're forwarded to the error middleware.
+    //* plainToInstance is safe outside try as it doesn't throw errors under normal conditions.
     try {
+      const errors = await validate(dto);
+      if (errors.length) {
+        console.error(errors);
+        throw new UserError(errors);
+      }
       const user = await this.userService.registerUser(dto);
       const token = genToken({
         id: user.id,
@@ -49,42 +49,42 @@ export class UserController {
         .status(201)
         .json({ user, token, emailVerificationToken, success: true });
     } catch (error) {
-      // TODO: handle this error
-      console.error(error);
-      res.status(500).send();
+      next(error);
     }
   }
 
-  async loginUser(req: Request, res: Response) {
-    const dto = Object.assign(new LoginUserDto(), req.body);
-    const errors = await validate(dto);
-    if (errors.length) {
-      res.status(400).json({ errors, success: false });
-      return;
-    }
-    const user = await this.userService.loginUser(dto);
-    const token = genToken({
-      id: user.id,
-      email: user.email,
-      isEmailVerified: user.isEmailVerified,
-      tokenType: Token.ACCESS,
-    });
-    res.status(200).json({ user, token, success: true });
-  }
+  async login(req: Request, res: Response, next: NextFunction) {
+    const dto = plainToInstance(LoginUserDto, req.body);
+    try {
+      const errors = await validate(dto);
+      if (errors.length) {
+        throw new UserError(errors);
+      }
 
-  async verifyEmail(req: Request, res: Response) {
-    const { email, tokenType } = req.body;
+      const user = await this.userService.loginUser(dto);
+      const token = genToken({
+        id: user.id,
+        email: user.email,
+        isEmailVerified: user.isEmailVerified,
+        tokenType: Token.ACCESS,
+      });
+      res.status(200).json({ user, token, success: true });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async verifyEmail(req: Request, res: Response, next: NextFunction) {
+    const { email } = req.body;
 
     try {
       const user = await this.userService.updateEmailVerification(email, true);
       res.status(200).json({ success: true, user });
     } catch (error) {
-      console.error("Error verifying email:", error);
-      res.status(500).json({ error: "Internal server error", success: false });
+      next(error);
     }
   }
 
-  async forgetPassword(req: Request, res: Response) {
+  async forgetPassword(req: Request, res: Response, next: NextFunction) {
     const { email } = req.body;
 
     const token = genToken({
@@ -109,30 +109,34 @@ export class UserController {
         success: true,
         message: "AN email was sent with password recovery instruction.",
       });
-    } catch (error: Error | any) {
-      // TODO: handle the error later on.
+    } catch (error) {
+      next(error);
     }
   }
 
-  async resetPassword(req: Request, res: Response) {
+  async resetPassword(req: Request, res: Response, next: NextFunction) {
     const { oldPassword, password, email, tokenType } = req.body;
 
-    const dto = Object.assign(new LoginUserDto(), {
+    const dto = plainToInstance(LoginUserDto, {
       email: req.body.email,
       password: req.body.password,
     });
-    const errors = await validate(dto);
-    if (errors.length) {
-      res.status(400).json({ errors, success: false });
-      return;
+    try {
+      const errors = await validate(dto);
+      if (errors.length) {
+        console.error(errors);
+        throw new UserError(errors);
+      }
+      await this.userService.resetPassword(
+        email,
+        password,
+        tokenType === Token.ACCESS ? oldPassword : null
+      );
+      res.status(200).json({
+        success: true,
+      });
+    } catch (error) {
+      next(error);
     }
-    await this.userService.resetPassword(
-      email,
-      password,
-      tokenType === Token.ACCESS ? oldPassword : null
-    );
-    res.status(200).json({
-      success: true,
-    });
   }
 }
