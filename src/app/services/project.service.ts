8d88581d9b351f087d2fb54b3e47bda1809839d4
Saba Izraqi <izraqisaba@gmail.com>
2025-05-25 01:48:05 +0300

    Add ProjectService

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,5 +1,30 @@
 import { injectable, inject } from "tsyringe";
-import { IProjectRepo } from "../../domain/IRepos/IProjectRepo";
-import { Project } from "../../domain/entities/project.entity";
-import { UserError, DBConstraintError, ServerError } from "../exceptions";
-import { create } from "domain";
+import {
+  FindProjectOptions,
+  IProjectRepo,
+} from "../../domain/IRepos/IProjectRepo";
+import {
+  CreateProjectDto,
+  UpdateProjectDTO,
+} from "../../domain/DTOs/projectDTO";
+import { Project } from "../../domain/entities";
+
+@injectable()
+export class ProjectService {
+  constructor(@inject("IProjectRepo") private projectRepo: IProjectRepo) {}
+
+  async create(dto: CreateProjectDto): Promise<Project> {
+    return this.projectRepo.create(dto);
+  }
+
+  async update(dto: UpdateProjectDTO): Promise<Project> {
+    return this.projectRepo.update(dto);
+  }
+
+  async delete(id: string): Promise<void> {
+    return this.projectRepo.delete(id);
+  }
+  async find(options: FindProjectOptions): Promise<Project[]> {
+    return this.projectRepo.find(options);
+  }
+}
