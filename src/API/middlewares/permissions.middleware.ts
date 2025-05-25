a0055590e0783364240955d9ca29b27db2097572
Saba Izraqi <izraqisaba@gmail.com>
2025-05-25 19:40:40 +0300

    feat (middleware): implement authorization middleware - needs revisit

--- src/API/middlewares/permissions.middleware.ts
+++ src/API/middlewares/permissions.middleware.ts
@@ -1,19 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import { ProjectPermission } from "../../domain/enums/types";
+export function restrictTo(allowedPermission: ProjectPermission) {
+  return function (req: Request, res: Response, next: NextFunction): void {
+    //TODO : permission/role is not exist in the token. I should fetch the role for the user in the project from the DB.
+    const permission = req.body.permission; 
 
-export function restrictToAdmin(
-  req: Request,
-  res: Response,
-  next: NextFunction
-): void {
-  const permission = req.body.permission;
+    if (permission < allowedPermission) {
+      res.status(403).json({
+        success: false,
+        message: "Forbidden: You do not have permission to perform this action",
+      });
+      return;
+    }
 
-  if (permission !== ProjectPermission.ADMINISTRATOR) {
-    res.status(403).json({
-      success: false,
-      message: "Forbidden: You do not have permission to perform this action",
-    });
-    return;
-  }
-  next();
+    next();
+  };
 }
