1bb94681d38890cb8868447981e297df386c4345
Mohammed Mojahad <mohammadmojahad15@gmail.com>
2025-05-27 20:17:56 +0300

    i made all commints hofoly every thing stasfied

--- src/infrastructure/database/data-source.ts
+++ src/infrastructure/database/data-source.ts
@@ -16,10 +16,10 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT || 5001),
+  port: Number(process.env.DB_PORT || 5432),
   username: process.env.DB_USERNAME || "postgres",
   password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "sprintify",
+  database: process.env.DB_NAME || "sprintify", 
   synchronize: true, // dev only! disable in prod
   logging: ["error", "warn"], // comment out in prod
   entities: [
